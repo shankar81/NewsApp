9fbcfab83cc3ba4ec3629eebfea694a8e0a841ce
shankarsawant81 <shankarsawant81@gmail.com>
2020-11-27 19:14:42 +0530

    Custom Tabs- Added Custom back button icon Warming up browsers added myBeLaunchUrl() Added enter/exit animation Connected to CustomTabsService

--- app/src/main/java/com/example/newsapp/NewsFragment.kt
+++ app/src/main/java/com/example/newsapp/NewsFragment.kt
@@ -1,12 +1,21 @@
 package com.example.newsapp
 
+import android.app.PendingIntent
+import android.content.ComponentName
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.view.inputmethod.EditorInfo
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
+import androidx.browser.customtabs.*
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,11 +23,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.newsapp.models.News
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+
+private const val CUSTOM_TAB_PACKAGE_NAME = "com.android.chrome"
+private const val TAG = "NewsFragment"
 
 class NewsFragment(private val category: String = "") : Fragment() {
 
@@ -30,12 +44,35 @@ class NewsFragment(private val category: String = "") : Fragment() {
     private var searchQuery = ""
     private var queryJob: Job? = null
 
+    private var mCustomTabsClient: CustomTabsClient? = null
+    private var mCustomTabsSession: CustomTabsSession? = null
+
+    // Custom tab service connection
+    private val connection = object : CustomTabsServiceConnection() {
+        override fun onServiceDisconnected(name: ComponentName?) {
+        }
+
+        override fun onCustomTabsServiceConnected(name: ComponentName, client: CustomTabsClient) {
+            mCustomTabsClient = client
+            mCustomTabsClient?.warmup(2)
+            mCustomTabsSession = mCustomTabsClient?.newSession(object : CustomTabsCallback() {
+
+            })
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = layoutInflater.inflate(R.layout.fragment_news, container, false)
+        // Binding to service
+        CustomTabsClient.bindCustomTabsService(
+            requireContext(),
+            CUSTOM_TAB_PACKAGE_NAME,
+            connection
+        )
 
         mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         setHasOptionsMenu(true)
@@ -116,8 +153,69 @@ class NewsFragment(private val category: String = "") : Fragment() {
             news.clear()
             news.addAll(newsList)
             adapter.notifyDataSetChanged()
+
+            val otherLikelyUrls = arrayListOf<Bundle>()
+            for (index in newsList.indices) {
+                if (index != 0) {
+                    val bundle = Bundle().apply {
+                        putParcelable(CustomTabsService.KEY_URL, Uri.parse(newsList[index].url))
+                    }
+                    otherLikelyUrls.add(bundle)
+                }
+            }
+            val res =
+                mCustomTabsSession?.mayLaunchUrl(Uri.parse(newsList[0].url), null, otherLikelyUrls)
+            Log.d(TAG, "onViewCreated: $res")
         })
         doAPICall()
+    }
+
+    private fun launchUrl(url: String?) {
+        if (url == null) {
+            Toast.makeText(context, "Url not found", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val uri = Uri.parse(url)
+        val pi = PendingIntent.getActivity(
+            context,
+            0,
+            Intent(context, MainActivity::class.java),
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val builder = CustomTabsIntent.Builder(mCustomTabsSession)
+
+        val color = ResourcesCompat.getColor(resources, R.color.purple_500, null)
+        builder.setToolbarColor(color)
+        builder.addDefaultShareMenuItem()
+
+        // Add menu item
+        builder.addMenuItem("Custom Menu Item", pi)
+
+        // Animation Enter/Exit
+        builder.setStartAnimations(
+            requireContext(),
+            R.anim.slide_left_in,
+            R.anim.slide_left_out
+        )
+        builder.setExitAnimations(
+            requireContext(),
+            R.anim.slide_right_in,
+            R.anim.slide_right_out,
+        )
+
+        // Add Custom Back Button Icon
+        Glide.with(requireContext()).asBitmap().load(R.drawable.icon_back).into(object :
+            CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                builder.setCloseButtonIcon(resource)
+                val customTabsIntent = builder.build()
+                customTabsIntent.launchUrl(requireContext(), uri)
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {
+            }
+        })
     }
 
     private inner class NewsHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -140,6 +238,10 @@ class NewsFragment(private val category: String = "") : Fragment() {
                 mainViewModel.addFavourite(news)
                 Toast.makeText(context, "Added to favourites", Toast.LENGTH_SHORT)
                     .show()
+            }
+
+            itemView.setOnClickListener {
+                launchUrl(news.url)
             }
         }
     }
